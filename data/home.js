19e195a2099dd874594146dddaf6535839410221
Niladri ;) <levio10002@gmail.com>
2024-05-11 16:08:51 +0530

    Add files via upload

---
+++ data/home.js
@@ -0,0 +1,292 @@
+export const offersAarray = [
+    {
+      image:
+        "https://images.pexels.com/photos/3550482/pexels-photo-3550482.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "144.37",
+      discount: "17",
+    },
+  
+    {
+      image:
+        "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "12.76",
+      discount: "19",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "4.09",
+      discount: "32",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3550482/pexels-photo-3550482.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "9.61",
+      discount: "90",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "6.25",
+      discount: "45",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3550482/pexels-photo-3550482.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "13.74",
+      discount: "10",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=600",
+      price: "198.2",
+      discount: "5",
+    },
+  ];
+  export const menuArray = [
+    {
+      name: "Women's Fashion",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Men's Fashion",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Electronics",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Jewlery & Watches",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Home, Pet & Appliances",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Beauty, Health & Hair",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+  
+    {
+      name: "Shoes,Sneakers,Heels",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+  
+    {
+      name: "Accessories",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+  
+    {
+      name: "Sports & Entertainment ",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Kids & Babies",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Movies & Television",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Gaming, Video Games",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Phones & Telecommunications",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Toys & Hobbies",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Gifts & Crafts",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Machinery",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+    {
+      name: "Security, Safety",
+      link: "",
+      subMenu: [
+        {
+          name: "",
+          link: "",
+        },
+      ],
+      brands: [],
+      images: [],
+    },
+  ];
+  
+export const userSwiperArray = [
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S8bf3306f6f7b4855b147862236a5a12dD/45MM-Transparent-Mod-Kit-Case-for-Apple-Watch-Band-7-45-44MM-Rubber-Sports-Watchband-for.jpg_220x220xz.jpg_.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/Sdb42f04658cd4a9c82018e22e90a78a2X.jpg_220x220.webp_480x480Q90.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/H5914a44106c44f4ca51280c4fcd0a3bfY.jpg_220x220.webp_480x480Q90.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/H5b92665c4bae4160845d4d235586a41b7/Win10-OneGX1-Pro-Mini-Laptop-Gaming-7-inch-Notebook-Computer-Intel-i7-16G-RAM-512G-PICe.jpg_220x220xz.jpg_.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/H01e60132bef1420a849fa84700ebae174.jpg_350x350.jpg_480x480Q90.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S714e9ef3689b4a31954f2d180d4938c9u.jpg_220x220.webp_480x480Q90.webp",
+    link: "",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S31f6384cf5ae41da86f9554615e7e392E.jpg_220x220.webp_480x480Q90.webp",
+    link: "",
+  },
+];
